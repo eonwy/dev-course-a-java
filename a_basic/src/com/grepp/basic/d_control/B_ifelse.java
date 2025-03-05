e8aece36588d7ce38042b714ae5b101bf0b3ca8b
kuma <yeawon6566@gmail.com>
2025-03-05 22:04:31 +0900

    0305 수업 마무리

--- a_basic/src/com/grepp/basic/d_control/B_ifelse.java
+++ a_basic/src/com/grepp/basic/d_control/B_ifelse.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class B_ifelse {
 
+    // NOTE DB01 양자택일 조건문
+    // if(조건식){조건식이 참일 때 코드블록}
+    // else{조건식이 거짓일 때 수행될 코드블록}
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("\n --- 인증 --- \n");
+        System.out.println("\n=== 인증 ===\n");
         String username = "김예원";
-        String storedPassword = "123456";
+        String storedPassword = "1234";
 
-        System.out.println("이름 : ");
+        System.out.print("이름: ");
         String name = sc.next();
-        System.out.println("비밀번호 : ");
+        System.out.print("비밀번호: ");
         String password = sc.next();
 
-        if (username.equals(name) && password.equals(storedPassword)) {
+        // 아이디와 비밀번호가 모두 일치하면 인증성공, 인증실패
+
+        if(username.equals(name) && storedPassword.equals(password)){
             System.out.println("환영합니다 " + name + "님!");
-        } else {
-            System.out.println("인증에 실패하였습니다.");
+        }else{
+            System.out.println("인증에 실패했습니다.");
         }
     }
 
